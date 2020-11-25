a7aa74b6fd55c6987ea260bd85283a09d6c4b1c5
AlexBulanov <alex-bulanov.mail@yandex.ru>
2020-11-25 19:20:52 +0300

    Домашка №5
    Сделал рефакторинг,
    Разбил всё по модулям,
    Создал класс Pokemon

---
+++ utils/checkHP.js
@@ -0,0 +1,3 @@
+export default function checkHP(person) {
+  return person.hp.current > 0 ? true : false;
+}
---
+++ utils/getRandomNumber.js
@@ -0,0 +1,3 @@
+export default function getRandomNumber(num) {
+  return Math.ceil(Math.random() * num);
+}
